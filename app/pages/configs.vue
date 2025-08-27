8820e3cc655924bcba053e037da4254f30988d09
Artem Filippow <a.filippov@qtech.ru>
2025-08-27 13:21:51 +0900

    price format

--- app/pages/configs.vue
+++ app/pages/configs.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import type {IConfig} from "~~/server/models/config.model";
-
+import type { QTableColumn } from 'quasar'
+const {$priceFormat} = useNuxtApp()
 const list = ref<IConfig[]>([])
 async function load(){
   list.value = await useNuxtApp().$GET('/config/list') as IConfig[]
 }
 onMounted(load)
-const columns = [
-  {field: 'date', label: 'Дата'},
-  {field: (p)=>p.platform.desc, label: 'Описание'},
-  {field: 'price', label: 'Сумма'},
+const columns : QTableColumn[] = [
+  {field: 'date', label: 'Дата', name:'data'},
+  {field: (row)=>row.platform.desc, label: 'Описание', name:'desc'},
+  {field: (row)=>$priceFormat(row.price), label: 'Сумма', name: 'price', style: 'text-align: right;'},
 ]
 </script>
 
