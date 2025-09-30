07a08520c0b0867dd95afba5466a1cfac4814f19
Artem Filippow <a.filippov@qtech.ru>
2025-09-30 15:56:15 +0900

    fix cache choice

--- app/pages/config/[id].vue
+++ app/pages/config/[id].vue
@@ -108,7 +108,7 @@ const editName = ref(true)
                       :disabled="['NMB-LCS-BASE', 'NMB-LCS-DCTPKG'].includes(item.article)"
                       @change="e=>addParts(partCount(item)?0:1, item)"
                       :value="partCount(item)")
-                    select(v-else-if="['JBD','-EF-'].includes(tab)" @change="e=>addParts(e.target.value, item)" :value="partCount(item)")
+                    select(v-else-if="['JBD','-EF-','-CH-'].includes(tab)" @change="e=>addParts(e.target.value, item)" :value="partCount(item)")
                       option(v-for="val in $partOptions(conf,tab)" :value="val") {{val}}
                     input(v-else @change="e=>addParts(e.target.value, item)" type="number" :value="partCount(item)" :min="0")
 
