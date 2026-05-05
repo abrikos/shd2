d7ff5c1f9b3a6d706989a2efb3ce2af0e738d937
Artem Filippow <a.filippov@qtech.ru>
2026-05-05 16:51:52 +0900

    fix pcie 8 + 16

--- app/plugins/logic.ts
+++ app/plugins/logic.ts
@@ -81,8 +81,8 @@ export default defineNuxtPlugin(() => {
                     list.push(`Недостаточно OCP слотов (${conf.platform.ocpMax}) для выбранного количества OCP устройств (${conf.ocpCount})`);
                 }
                 console.log(conf.platform.pcie8Max, conf.pcie8Count, '===', conf.platform.pcie16Max , conf.pcie16Count)
-                if(conf.platform.pcie8Max < conf.pcie8Count && conf.platform.pcie16Max <= conf.pcie16Count){
-                    list.push(`Недостаточно PCIE слотов (${conf.platform.pcie8Max}) для выбранного количества устройств (${conf.pcie8Count})`);
+                if(conf.platform.pcie8Max + conf.platform.pcie16Max < conf.pcie8Count){
+                    list.push(`Недостаточно PCIE слотов (${conf.platform.pcie8Max + conf.platform.pcie16Max}) для выбранного количества устройств (${conf.pcie8Count})`);
                 }
                 if(conf.platform.pcie16Max < conf.pcie16Count){
                     list.push(`Недостаточно PCIE16 слотов (${conf.platform.pcie16Max}) для выбранного количества устройств (${conf.pcie16Count})`);
